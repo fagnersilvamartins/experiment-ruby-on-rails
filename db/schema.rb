2a1f7e7902ab403180a993f7b75decd304c3eeb5
Fagner Silva Martins <fagner.martins@embedded.ufcg.edu.br>
2017-08-02 18:40:10 -0300

    Remove seach

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802212400) do
+ActiveRecord::Schema.define(version: 20170802213219) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
